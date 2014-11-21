54b5c938667d137caad6e3d1e4a66b0d8597626e
Alex Mantel <alexmantel93@gmail.com>
2014-11-21 17:29:38 +0100

    Add tests for from_string

---
+++ unit_testing/test_from_string.cpp
@@ -0,0 +1,57 @@
+#include "test.hpp"
+
+#include "caf/all.hpp"
+
+#include <iostream>
+
+using namespace std;
+using namespace caf;
+
+int main() {
+  CAF_TEST(test_from_string);
+  {
+    auto val = from_string<message>("@<>+@i32 ( 42 )");
+    CAF_CHECK(val == make_message(42));
+    CAF_CHECK(val == from_string<message>(to_string(*val)));
+  }
+  {
+    string str = "atom";
+    auto val = from_string<message>("@<>+@atom ( '" + str + "' )");
+    CAF_CHECK(val == make_message(atom("atom")));
+    CAF_CHECK(val == from_string<message>(to_string(*val)));
+  }
+  {
+    auto val = from_string<message>("@<>+bool ( true )");
+    CAF_CHECK(val == make_message(true));
+    CAF_CHECK(val == from_string<message>(to_string(*val)));
+  }
+  {
+    uint32_t a = 5;
+    uint64_t b = 9;
+    auto val = from_string<message>("@<>+u32+u64 (5, 9)");
+    CAF_CHECK(val == make_message(a, b));
+  }
+  {
+    auto val = from_string<message>("@<>+@atom+@i32+@i32 ( 'add' 5 10 )");
+    CAF_CHECK(val == make_message(atom("add"), 5, 10));
+  }
+  {
+    // uint32_t
+    auto val = from_string<message>("42");
+    CAF_CHECK(val == make_message(42));
+  }
+  {
+    auto val = from_string<message>("'atom' 'atom'");
+    CAF_CHECK(val == make_message(atom("atom"), atom("atom")));
+  }
+  {
+    auto val = from_string<message>("true");
+    CAF_CHECK(val == make_message(true)); 
+  }
+  {
+    auto val = from_string<message>("'add' 5 10");
+    CAF_CHECK(val == make_message(atom("add"), 5, 10));
+    CAF_CHECK(val == from_string<message>(to_string(*val)));
+  }
+  return CAF_TEST_RESULT();
+}
